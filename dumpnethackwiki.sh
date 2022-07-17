372a0060d9cae01af23fbe10cba7b44c29aac394
Roman Garanin <git@romangaranin.net>
2022-07-17 15:44:30 +0300

    feat: return back original email

--- dumpnethackwiki.sh
+++ dumpnethackwiki.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker volume create nethackwikizim
-docker run --rm -v=nethackwikizim:/out --name=mwoffliner openzim/mwoffliner mwoffliner --verbose --mwUrl=https://nethackwiki.com/ --adminEmail=example@example.com --outputDirectory=/out
+docker run --rm -v=nethackwikizim:/out --name=mwoffliner openzim/mwoffliner mwoffliner --verbose --mwUrl=https://nethackwiki.com/ --adminEmail=tmp6154@yandex.ru --outputDirectory=/out
 docker container create --name temp -v nethackwikizim:/out busybox
 docker cp temp:/out .
 docker rm temp
